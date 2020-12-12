014188b62bc4755447cf0985cf94e1bc5952442e
Johns Jijo Sabu <jijojohns12.5@gmail.com>
2020-12-12 20:40:45 +0530

    Serailize extra data, Filtering & Pagination

---
+++ demo/store/api_views.py
@@ -0,0 +1,34 @@
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from django.utils import timezone
+from store.models import Product
+from store.serializers import ProductSerializer
+
+class ProductsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
+
+class ProductList(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter,)
+    filter_fields = ('id',)
+    search_fields = ('name', 'description',)
+    pagination_class = ProductsPagination
+    
+    def get_queryset(self):
+        on_sale = self.request.query_params.get('on_sale', None)
+        if on_sale is None:
+            return super().get_queryset()
+        queryset = Product.objects.all()
+        if on_sale.lower() == 'true':
+            now = timezone.now()
+            return queryset.filter(
+                sale_start__lte=now,
+                sale_end__gte=now,
+                )
+        return queryset
+
+        
